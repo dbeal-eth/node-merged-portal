3f908f53d8f8f868d44f520bbfae423c3d40fb2b
Daniel Beal <killerbyte@xram.co>
2014-07-31 11:24:40 -0700

    Add new user page

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,25 @@ var router = express.Router();
 
 var db = require('../db');
 
+var utils = require('../utils');
+
 /* GET users prompt */
 router.get('/', function(req, res) {
-    res.send('Use /users/<username> to look at your stats');
+    res.send('Use /user/<username> to look at your stats');
 });
 
 /* GET user specific statistics */
 router.get('/:user', function(req, res) {
     db.getUserStats(allCoins, req.params.user, function(data) {
-        if(!data) res.send('User does not exist');
-        else res.send(JSON.stringify(data, null, 4));
+        /*if(!data) res.send('User does not exist');
+        else res.send(JSON.stringify(data, null, 4));*/
+        if(!data) {
+            res.send('User not found');
+            return;
+        }
+        data.name = req.params.user;
+        data.hashrate = utils.hashrateString(data.hashrate);
+        res.render('user', data);
     });
 });
 
